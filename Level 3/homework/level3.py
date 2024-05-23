d040783539f9d477ca3ac0e758664c777bc17a43
lukadiasamidze <luka.dias2010@gmail.com>
2024-05-23 22:05:19 +0400

    commit

--- Level 3/homework/level3.py
+++ Level 3/homework/level3.py
@@ -1,3 +1,5 @@
+
+#1) შექმენი ცვლადები,  სახელი,გვარი,ასაკი,ჰობი,ქვეყანა და  ეს ყველა ცვლადი დაპრინტეთ. 
 name = "luka"
 last_name = "diasamidze"
 age = "13"
@@ -10,6 +12,7 @@ print(age)
 print(hobby)
 print(country)
 
+#2) შექმენით ცვლადები, გადაეცით მნიშვნელობები და შემდგომ დაამატეთ ერთმანეთზე.
 number1 = 1
 number2 = 2
 number3 = 3
@@ -18,14 +21,66 @@ number5 = 5
 
 print(number1 + number2 + number3 + number4 + number5)
 
-print(number1 * number2)
+#3)  გააკეთეთ 10 მაგალითი ყველა მათემათიკურ ოპერატორებზე. +,-,*,//,**
+print(number1 // number2)
 print(number1 // number3)
-print(number1 + number4)
-print(number2 * number5)
+print(number1 // number4)
+print(number2 // number5)
 print(number5 // number2)
-print(number2 * number1)
-print(number3 - number2)
-print(number1 + number5)
+print(number2 // number1)
+print(number3 // number2)
+print(number1 // number5)
 print(number4 // number1)
-print(number2 + number1)
+print(number2 // number1)
+print(number4 // number5)
+
+print(number1 - number2)
+print(number1 - number3)
+print(number1 - number4)
+print(number2 - number5)
+print(number5 - number2)
+print(number2 - number1)
+print(number3 - number2)
+print(number1 - number5)
+print(number4 - number1)
+print(number2 - number1)
 print(number4 - number5)
+
+
+print(number1 + number2)
+print(number1 + number3)
+print(number1 + number4)
+print(number2 + number5)
+print(number5 + number2)
+print(number2 + number1)
+print(number3 + number2)
+print(number1 + number5)
+print(number4 + number1)
+print(number2 + number1)
+print(number4 + number5)
+
+
+print(number1 * number2)
+print(number1 * number3)
+print(number1 * number4)
+print(number2 * number5)
+print(number5 * number2)
+print(number2 * number1)
+print(number3 * number2)
+print(number1 * number5)
+print(number4 * number1)
+print(number2 * number1)
+print(number4 * number5)
+
+
+print(number1 ** number2)
+print(number1 ** number3)
+print(number1 ** number4)
+print(number2 ** number5)
+print(number5 ** number2)
+print(number2 **number1)
+print(number3 ** number2)
+print(number1 ** number5)
+print(number4 ** number1)
+print(number2 ** number1)
+print(number4 ** number5)
