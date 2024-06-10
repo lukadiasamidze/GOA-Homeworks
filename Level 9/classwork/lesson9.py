cf7dad2937c782310aefc877fdad480fe2892a90
lukadiasamidze <luka.dias2010@gmail.com>
2024-06-10 17:23:09 +0400

    commit

---
+++ Level 9/classwork/lesson9.py
@@ -0,0 +1,18 @@
+# ask the user for input and store it in a variable
+name = input()
+
+# display the user input on the screen
+
+print()
+
+# take the name as input
+
+name = input("name: ")
+
+# use concatenation to join 2 strings
+
+message = "Nice to meet you, " + name
+
+# display the message to the user
+
+print(message)
---
+++ lesson9.py
@@ -0,0 +1,18 @@
+number1 = input("Enter first number: ")
+number2 = input("Enter second number: ")
+
+print(int(number1) + int(number2))
+
+x = input("Enter the lenght: ")
+y = input("Enter the height: ")
+
+print(int(x * 2) + int(y * 2))
+
+number3 = input("Enter the number that will be multiplied by 2: ")
+
+print(int(number3) * 2)
+
+number4 = input("Enter number: ")
+
+print(float(number4) * 2)
+
