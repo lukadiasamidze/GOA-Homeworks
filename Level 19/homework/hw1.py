b2ec67e1ba2831f471aecfeac200c2b675354528
lukadiasamidze <luka.dias2010@gmail.com>
2024-07-23 14:10:37 +0400

    commit

---
+++ Level 19/homework/hw1.py
@@ -0,0 +1,4 @@
+# Print numbers from 126 to 0, use For loop
+
+for i in range(126,0,-1):
+    print(i)
---
+++ Level 19/homework/hw5.py
@@ -0,0 +1,6 @@
+#Print number from 1 to 100 by using While loop
+
+count = 1
+while count<=100:
+    print(count)
+    count += 1
